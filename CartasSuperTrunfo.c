baee445739e638ac4b7a421ff597228dbf583530
1344-dias <p.sandra718@gmail.com>
2026-03-01 18:17:41 +0000

    Fiz o desafio nível novato, fazendo os ajustes. Adicionei Printf no código Ajustei algumas variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,44 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  char codigo[4];
+  char cidade[50];
+  int populacao;
+  float area;
+  float pib;
+  int pontosturisticos;
+  
+  
   // Área para entrada de dados
 
-  // Área para exibição dos dados da cidade
+  printf("Digite o codigo da carta: ");
+  scanf("%s", codigo);
 
-return 0;
-} 
+  printf("Digite a cidade: ");
+  scanf("%s", cidade);
+  
+  printf("Digite a população da cidade: ");
+  scanf("%d", &populacao);
+
+  printf("Digite a area da cidade km²:\n" );
+  scanf(" %f", &area);
+
+  printf("Digite o pib da cidade:\n");
+  scanf(" %f", &pib);
+
+  printf("digite o numero de pontos turisticos:\n");
+  scanf(" %d", &pontosturisticos);
+
+
+    // Área para exibição dos dados da cidade
+
+    printf("\n -  Dados da Carta - \n");
+    printf("Codigo: %s\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f km²\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontosturisticos: %d\n", pontosturisticos);
+
+    return 0;
+}
